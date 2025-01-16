b7c7ec6f9387ae4371cc8615ea9f0f6e05e77215
TylerSchaus <tylerdschaus@shaw.ca>
2025-01-15 19:19:36 -0800

    Basic game complete, no jumping yet

---
+++ COSC_416_Studio_1/Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();  
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 inputVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        OnMove?.Invoke(inputVector); 
+
+
+    }
+}
